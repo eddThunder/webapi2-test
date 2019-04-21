d77119582d8fdc8499542b559c0216783f313c87
MORET, EDUARDO <emoret@itteria.com>
2019-04-21 12:15:54 +0200

    fix scripts

--- SQLUsers/Scripts/UsersLoad.sql
+++ SQLUsers/Scripts/UsersLoad.sql
@@ -6,9 +6,9 @@ INSERT INTO [dbo].[Users] ([Id], [Username], [UserPassword]) VALUES (2, N'nuria'
 INSERT INTO [dbo].[Users] ([Id], [Username], [UserPassword]) VALUES (3, N'gabri', N'farigola')
 INSERT INTO [dbo].[Users] ([Id], [Username], [UserPassword]) VALUES (4, N'manu', N'machupichu')
 INSERT INTO [dbo].[Users] ([Id], [Username], [UserPassword]) VALUES (5, N'Zigor', N'huhui')
-INSERT INTO [dbo].[Users] ([Id], [Username], [UserPassword]) VALUES (2003, N'Donald Duck', N'hehei')
-INSERT INTO [dbo].[Users] ([Id], [Username], [UserPassword]) VALUES (2005, N'Cheewbaka', N'hohoi')
-INSERT INTO [dbo].[Users] ([Id], [Username], [UserPassword]) VALUES (3001, N'Marlo', NULL)
+INSERT INTO [dbo].[Users] ([Id], [Username], [UserPassword]) VALUES (6, N'Donald Duck', N'hehei')
+INSERT INTO [dbo].[Users] ([Id], [Username], [UserPassword]) VALUES (7, N'Cheewbaka', N'hohoi')
+INSERT INTO [dbo].[Users] ([Id], [Username], [UserPassword]) VALUES (8, N'Marlo', NULL)
 SET IDENTITY_INSERT [dbo].[Users] OFF
 
 
--- SQLUsers/Scripts/UsersRolesLoad.sql
+++ SQLUsers/Scripts/UsersRolesLoad.sql
@@ -1,15 +1,17 @@
 ﻿
 
 SET IDENTITY_INSERT [dbo].[UsersRoles] ON
-INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (5, 1, 1)
-INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (6, 1, 2)
-INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (7, 1, 3)
-INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (8, 1, 4)
-INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (4003, 5, 1)
-INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (4004, 5, 2)
-INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (4002, 5, 4)
-INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (2004, 2003, 3)
-INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (3004, 2005, 2)
-INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (3005, 2005, 4)
-INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (4005, 3001, 3)
+INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (1, 1, 1)
+INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (2, 1, 2)
+INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (3, 1, 3)
+INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (4, 1, 4)
+INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (5, 5, 1)
+INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (6, 5, 2)
+INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (7, 5, 4)
+INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (8, 2, 3)
+INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (9, 3, 2)
+INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (10, 4, 4)
+INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (11, 6, 3)
+INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (11, 7, 1)
+INSERT INTO [dbo].[UsersRoles] ([Id], [UserId], [RoleId]) VALUES (11, 8, 2)
 SET IDENTITY_INSERT [dbo].[UsersRoles] OFF
